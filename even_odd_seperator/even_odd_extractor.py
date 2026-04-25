4a20b89eb5030669b1ed7fb716b467c7b7097ce1
Adeinr22 <andrei05.jules22@gmail.com>
2026-04-26 01:12:19 +0800

    Introduced error handling to random integer generator

--- even_odd_seperator/even_odd_extractor.py
+++ even_odd_seperator/even_odd_extractor.py
@@ -11,14 +11,24 @@ class EvenOddExtractor():
         while True:
             try:
                 numbers_to_generate = int(input("How many integers you want to generate?\n"))
-                with open(path.join(path.dirname(path.abspath(__file__)), "numbers.txt"), "a") as f:
-                    for i in range(numbers_to_generate):
-                        if i == (numbers_to_generate - 1):
-                            f.write(f"{randint(-1000000, 1000000)}")
-                        else:
-                            f.write(f"{randint(-1000000, 1000000)}\n")
-                break
+                if numbers_to_generate == 0:
+                    break
+                elif numbers_to_generate <= 0:
+                    print("can't go negative")
+                elif numbers_to_generate == 1:
+                    with open(path.join(path.dirname(path.abspath(__file__)), "numbers.txt"), "a") as f:
+                        f.write(f"{randint(-1000000, 1000000)}")
+                    break
+                elif numbers_to_generate >= 1:
+                    with open(path.join(path.dirname(path.abspath(__file__)), "numbers.txt"), "a") as f:
+                        for i in range(numbers_to_generate):
+                            if i == (numbers_to_generate - 1):
+                                f.write(f"{randint(-1000000, 1000000)}")
+                            else:
+                                f.write(f"{randint(-1000000, 1000000)}\n")
+                    break
             except ValueError:
                 print("Input must only be an integer")
 
 EvenOddExtractor.data_clearer()
+EvenOddExtractor.random_integer_generator()
