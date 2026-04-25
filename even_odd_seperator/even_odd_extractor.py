129a5d940ad520f248a95d5450e44dab7250165c
Adeinr22 <andrei05.jules22@gmail.com>
2026-04-26 02:55:14 +0800

    Simplified EvenOddExtractor and makes it communicate with main.py. Allows for generating and clearing of file without the need to close and rerun

--- even_odd_seperator/even_odd_extractor.py
+++ even_odd_seperator/even_odd_extractor.py
@@ -4,36 +4,53 @@ class EvenOddExtractor():
 
     def data_clearer(chosen_file):
         """Clears all data in chosen file"""
-        with open(path.join(path.dirname(path.abspath(__file__)), f"{chosen_file}"), "w"):
+        with open(path.join(path.dirname(path.abspath(__file__)), chosen_file), "w"):
             pass
 
     def data_remover():
-        pass
-        
-        
-    def random_integer_generator():
-        from random import randint
+        """Remove data in a specific index or range of lines"""
         while True:
-            try:
-                numbers_to_generate = int(input("How many integers you want to generate?\n"))
-                if numbers_to_generate == 0:
+            what_to_remove = input('Do you want to remove a data in a specific index (1) or a range of lines (2)? (enter "C" to cancel) \n')
+            if what_to_remove == "C" or "c":
+                break
+            elif what_to_remove == "1":
+                while True:
+                    index_list = []
+                    index_delete = input("Enter the index you want to delete ('N' to stop / 'C' to cancel): ")
+                    if index_delete != 'C' or 'c' or 'N' or 'n' or not index_delete.isdigit:
+                        print("invalid input")
+                    if index_delete == 'C' or 'c':
+                        index_list.clear
+                        break
+                    if index_delete == 'N' or 'n':
+                        break
+                    else:
+                        index_list.append(index_delete)
+                if index_list == []:
                     break
-                elif numbers_to_generate <= 0:
-                    print("can't go negative")
-                elif numbers_to_generate == 1:
-                    with open(path.join(path.dirname(path.abspath(__file__)), "numbers.txt"), "a") as f:
-                        f.write(f"{randint(-1000000, 1000000)}")
-                    break
-                elif numbers_to_generate >= 1:
-                    with open(path.join(path.dirname(path.abspath(__file__)), "numbers.txt"), "a") as f:
-                        for i in range(numbers_to_generate):
-                            if i == (numbers_to_generate - 1):
-                                f.write(f"{randint(-1000000, 1000000)}")
-                            else:
-                                f.write(f"{randint(-1000000, 1000000)}\n")
-                    break
-            except ValueError:
-                print("Input must only be an integer")
+                else:
+                    pass
 
-EvenOddExtractor.data_clearer()
-EvenOddExtractor.random_integer_generator()
+            elif what_to_remove == "2":
+                pass
+            else:
+                print("input only 1 or 2")
+            pass
+        
+        
+    def random_integer_generator(numbers_to_generate):
+        from random import randint
+        if numbers_to_generate == 0:
+            pass
+        elif numbers_to_generate <= 0:
+            print("can't go negative")
+        elif numbers_to_generate == 1:
+            with open(path.join(path.dirname(path.abspath(__file__)), "numbers.txt"), "a") as f:
+                f.write(f"{randint(-1000000, 1000000)}")
+        elif numbers_to_generate >= 1:
+            with open(path.join(path.dirname(path.abspath(__file__)), "numbers.txt"), "a") as f:
+                for i in range(numbers_to_generate):
+                    if i == (numbers_to_generate - 1):
+                        f.write(f"{randint(-1000000, 1000000)}")
+                    else:
+                        f.write(f"{randint(-1000000, 1000000)}\n")
--- main.py
+++ main.py
@@ -1,2 +1,47 @@
-from even_odd_seperator import even_odd_extractor
+from even_odd_seperator.even_odd_extractor import EvenOddExtractor
 
+while True:
+
+    program_1 = input("""
+MENU:
+                      
+    (1) Clear Data
+    (2) Remove Data
+    (3) Generate Integers
+    (C) Cancel
+                      
+what to do: """)
+    print()
+    
+    if program_1 == '1':
+        while True:
+            file_choosed = input("Enter the number for the file you want to clear: (1) numbers.txt | (2) even.txt | (3) odd.txt | (C) to cancel \n")
+            if file_choosed == 'C':
+                break
+            elif file_choosed == 'c':
+                break
+            elif file_choosed == '1':
+                EvenOddExtractor.data_clearer("numbers.txt")
+            elif file_choosed == '2':
+                EvenOddExtractor.data_clearer("even.txt")
+            elif file_choosed == '3':
+                EvenOddExtractor.data_clearer("odd.txt")
+            else:
+                print('Invalid Input.')
+
+    elif program_1 == '3':
+        while True:
+            numbers_to_generate = input("How many integers you want to generate? ('C' to Cancel)\n")
+            try:
+                if numbers_to_generate == 'C':
+                    break
+                elif int(numbers_to_generate):
+                    EvenOddExtractor.random_integer_generator(int(numbers_to_generate))
+            except:
+                print("Invalid Input")
+
+    elif program_1 == 'C':
+        break
+
+    else:
+        print("invalid input")
