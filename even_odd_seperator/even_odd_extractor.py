dd9af15bfdf64348d2ec8f33937d0d9a77a539e2
Adeinr22 <andrei05.jules22@gmail.com>
2026-04-28 22:10:33 +0800

    data_remover function finished plus some minor improvements

--- even_odd_seperator/even_odd_extractor.py
+++ even_odd_seperator/even_odd_extractor.py
@@ -11,17 +11,13 @@ class EvenOddExtractor():
         """Remove data in a specific lines or range of lines"""
         while True:
             what_to_remove = input('Do you want to remove a data in a specific lines (1) or a range of lines (2)? (enter "C" to cancel) \n')
-            if what_to_remove == "C":
-                break
-            if what_to_remove == "c":
+            if what_to_remove.lower() == "c":
                 break
             elif what_to_remove == "1":
                 while True:
                     line_delete = input("Enter the line you want to delete ('C' to cancel): ")
                     if line_delete.isalpha():
-                        if line_delete == 'C':
-                            break
-                        elif line_delete == 'c':
+                        if line_delete.lower() == 'c':
                             break
                         else:
                             print("invalid input")
@@ -38,10 +34,43 @@ class EvenOddExtractor():
                         print(f"Invalid input. line {line_delete} does not exist. file has {len(all_lines)} lines.")
                     lines.clear()
             elif what_to_remove == "2":
-                pass
+                run_indicator = True
+                if run_indicator:
+                    while True:
+                        start_delete = input("Enter the start of the line you want to delete ('C' to cancel): ")
+                        if start_delete.lower() == 'c':
+                            run_indicator = False
+                            break
+                        elif start_delete.isdigit():
+                            if int(start_delete) == 0:
+                                print("There is no line 0. it starts from line 1")
+                            else:
+                                break
+                        else:
+                            print("invalid input")
+                if run_indicator:
+                    while True:
+                        end_delete = input("Enter the end of the line you want to delete ('C' to cancel): ")
+                        if end_delete.lower() == 'c':
+                            run_indicator = False
+                            break
+                        elif end_delete.isdigit():
+                            if int(start_delete) == int(end_delete):
+                                print("starting line and end line must not be the same.")
+                            elif int(start_delete) >= int(end_delete):
+                                print("ending line number must be higher than the starting line")
+                            else:
+                                break
+                        else:
+                            print("invalid input")
+                if run_indicator:
+                    with open(path.join(path.dirname(path.abspath(__file__)), chosen_file), "r") as f:
+                        lines = f.readlines()
+                    new_lines = [line for idx, line in enumerate(lines, start=1) if not (int(start_delete) <= idx <= int(end_delete))]
+                    with open(path.join(path.dirname(path.abspath(__file__)), chosen_file), "w") as f:
+                        f.writelines(new_lines)               
             else:
-                print("input only 1 or 2 or C")
-            pass
+                print("Invalid input. Only 1 or 2 or C are allowed")
         
         
     def random_integer_generator(numbers_to_generate):
@@ -53,11 +82,11 @@ class EvenOddExtractor():
             print("can't go negative")
         elif numbers_to_generate == 1:
             with open(path.join(path.dirname(path.abspath(__file__)), "numbers.txt"), "a") as f:
-                f.write(f"{randint(-1000000, 1000000)}")
+                f.write(f"{randint(-1000, 1000)}")
         elif numbers_to_generate >= 1:
             with open(path.join(path.dirname(path.abspath(__file__)), "numbers.txt"), "a") as f:
                 for i in range(numbers_to_generate):
                     if i == (numbers_to_generate - 1):
-                        f.write(f"{randint(-1000000, 1000000)}")
+                        f.write(f"{randint(-1000, 1000)}")
                     else:
-                        f.write(f"{randint(-1000000, 1000000)}\n")
+                        f.write(f"{randint(-1000, 1000)}\n")
