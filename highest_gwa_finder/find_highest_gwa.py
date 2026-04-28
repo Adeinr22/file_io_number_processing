53a6093ee4bb6ade04e98e6d358004dac96438f4
Adeinr22 <andrei05.jules22@gmail.com>
2026-04-28 23:33:42 +0800

    created the StudentRecords class

--- highest_gwa_finder/find_highest_gwa.py
+++ highest_gwa_finder/find_highest_gwa.py
@@ -8,3 +8,9 @@ class Student():
 
     def __repr__(self):
         return f"student(name='{self.name}', gwa={self.gwa})"
+    
+class StudentRecords:
+    """Manages a collection of students and provides queries."""
+    def __init__(self, file_path: str):
+        self.students = []
+        self.load_from_file(file_path)
