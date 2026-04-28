0044296db10b323d99a02e59237df2fe48d488b4
Adeinr22 <andrei05.jules22@gmail.com>
2026-04-28 23:52:47 +0800

    improved main.py by making the run of program 1 a function

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from even_odd_seperator.even_odd_extractor import EvenOddExtractor
 
-while True:
-    program_chooser = input("""
-MAIN MENU:
-    (1) Even and Odd seperator program
-    (2) Highest GWA finder
-                            
-    choose program to run: """)
-while True:
-
-    program_1 = input("""
+def run_even_odd_program():
+    """Even and Odd separator submenu."""
+    while True:
+        program_1 = input("""
 MENU:
                       
     (1) Clear Data
@@ -21,63 +15,78 @@ MENU:
     (C) Cancel
                       
 what to do: """)
-    print()
-    
-    if program_1 == '1':
-        while True:
-            file_choosed = input("(1) numbers.txt | (2) even.txt | (3) odd.txt | (C) to cancel \nEnter the number for the file you want to remove data from: ")
-            if file_choosed == 'C':
-                break
-            elif file_choosed == 'c':
-                break
-            elif file_choosed == '1':
-                EvenOddExtractor.data_clearer("numbers.txt")
-            elif file_choosed == '2':
-                EvenOddExtractor.data_clearer("even.txt")
-            elif file_choosed == '3':
-                EvenOddExtractor.data_clearer("odd.txt")
-            else:
-                print('Invalid Input.')
+        print()
 
-    elif program_1 == '2':
-        while True:
-            file_choosed = input("(1) numbers.txt | (2) even.txt | (3) odd.txt | (C) to cancel \nEnter the number for the file you want to remove data from: ")
-            if file_choosed == 'C':
-                break
-            elif file_choosed == 'c':
-                break
-            elif file_choosed == '1':
-                EvenOddExtractor.data_remover("numbers.txt")
-            elif file_choosed == '2':
-                EvenOddExtractor.data_remover("even.txt")
-            elif file_choosed == '3':
-                EvenOddExtractor.data_remover("odd.txt")
-            else:
-                print('Invalid Input.')
-
-
-    elif program_1 == '3':
-        while True:
-            numbers_to_generate = input("How many integers you want to generate? ('C' to Cancel)\n")
-            try:
-                if numbers_to_generate == 'C':
+        if program_1 == '1':
+            while True:
+                file_choosed = input("(1) numbers.txt | (2) even.txt | (3) odd.txt | (C) to cancel \nEnter the number for the file you want to remove data from: ")
+                if file_choosed in ('C', 'c'):
                     break
-                elif int(numbers_to_generate):
-                    EvenOddExtractor.random_integer_generator(int(numbers_to_generate))
-            except:
-                print("Invalid Input")
-    
-    elif program_1 == '4':
-        EvenOddExtractor.even_extractor()
+                elif file_choosed == '1':
+                    EvenOddExtractor.data_clearer("numbers.txt")
+                elif file_choosed == '2':
+                    EvenOddExtractor.data_clearer("even.txt")
+                elif file_choosed == '3':
+                    EvenOddExtractor.data_clearer("odd.txt")
+                else:
+                    print('Invalid Input.')
 
-    elif program_1 == '5':
-        EvenOddExtractor.odd_extractor()
+        elif program_1 == '2':
+            while True:
+                file_choosed = input("(1) numbers.txt | (2) even.txt | (3) odd.txt | (C) to cancel \nEnter the number for the file you want to remove data from: ")
+                if file_choosed in ('C', 'c'):
+                    break
+                elif file_choosed == '1':
+                    EvenOddExtractor.data_remover("numbers.txt")
+                elif file_choosed == '2':
+                    EvenOddExtractor.data_remover("even.txt")
+                elif file_choosed == '3':
+                    EvenOddExtractor.data_remover("odd.txt")
+                else:
+                    print('Invalid Input.')
 
-    elif program_1 == '6':
-        EvenOddExtractor.extract_both()
+        elif program_1 == '3':
+            while True:
+                numbers_to_generate = input("How many integers you want to generate? ('C' to Cancel)\n")
+                try:
+                    if numbers_to_generate in ('C', 'c'):
+                        break
+                    elif int(numbers_to_generate):
+                        EvenOddExtractor.random_integer_generator(int(numbers_to_generate))
+                        break
+                except ValueError:
+                    print("Invalid Input")
 
-    elif program_1 == 'C':
+        elif program_1 == '4':
+            EvenOddExtractor.even_extractor()
+
+        elif program_1 == '5':
+            EvenOddExtractor.odd_extractor()
+
+        elif program_1 == '6':
+            EvenOddExtractor.extract_both()
+
+        elif program_1 in ('C', 'c'):
+            break
+
+        else:
+            print("invalid input")
+
+while True:
+    program_chooser = input("""
+MAIN MENU:
+    (1) Even and Odd separator program
+    (2) Highest GWA finder
+    (Q) Quit
+                            
+Choose program to run: """)
+
+    if program_chooser == '1':
+        run_even_odd_program()
+    elif program_chooser == '2':
+        pass
+    elif program_chooser in ('Q', 'q'):
+        print("Exiting program. Goodbye!")
         break
-
     else:
-        print("invalid input")
+        print("Invalid choice. Please enter 1, 2, or Q.")
